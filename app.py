7355d66226d0ba63dc18a7fc4b00552152fba56f
wallace-pv <wallace-pv@hotmail.com>
2025-04-11 18:33:13 -0300

    Last

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-import os
 
-app = Flask(name)
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     prompt = f"Responda de forma simples, objetiva e sempre em português, seja sempre muito educado, sem exageros: {data['prompt']}"
-
+    
     response = requests.post(
         "http://localhost:11434/api/generate",
         headers={"Content-Type": "application/json"},
@@ -24,6 +23,5 @@ def chat():
     result = response.json()
     return jsonify({"response": result["response"]})
 
-if name == "main":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)

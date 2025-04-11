113f0c126ba7aca93edfd9a384d7811f699b0d78
Rodrigo <Rodrigo_troncoso@live.com>
2025-04-11 17:38:48 -0300

    Ip fixo teste

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -24,5 +25,5 @@ def chat():
     return jsonify({"response": result["response"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

a0e87af8b15ffc6aa7e4305c3304b14c9d1c9f3e
wallace-pv <wallace-pv@hotmail.com>
2025-04-11 17:56:43 -0300

    Reorganizando Deploy

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
-app = Flask(__name__)
+app = Flask(name)
 CORS(app)
 
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     prompt = f"Responda de forma simples, objetiva e sempre em português, seja sempre muito educado, sem exageros: {data['prompt']}"
-    
+
     response = requests.post(
         "http://localhost:11434/api/generate",
         headers={"Content-Type": "application/json"},
@@ -23,6 +24,6 @@ def chat():
     result = response.json()
     return jsonify({"response": result["response"]})
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
